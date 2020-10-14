191988cff412db071db441d83af38b2a94bd8e13
JuanRcl <juanp.ribeirorcl@gmail.com>
2020-10-13 21:54:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+    <link rel="stylesheet" href="/css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300;1,400&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="divPrincipal">
+        <h2>    
+            Tela de saída
+        </h2>
+        <?php
+            $nome = "Juan Pablo";
+            $idade = 20; 
+            echo "$nome tem $idade anos <br\>";
+            
+        ?>
+        <h4><a id="links" href="/exercicios/ex01.php">Exercício 01</a></h4>
+        <h4><a id="links" href="/exercicios/ex02html.html">Exercício 02</a></h4>
+        <h4><a id="links" href="/exercicios/ex03html.html">Exercício 03</a></h4>
+        <h4><a id="links" href="/exercicios/ex04html.html">Exercício 04</a></h4>
+        <h4><a id="links" href="/exercicios/ex05php.php">Exercício 05</a></h4>
+        <h4><a id="links" href="/exercicios/ex06-funcao.php">Exercício 06</a></h4>
+        <h4><a id="links" href="/exercicios/ex07-funcoes2.php">Exercício 07</a></h4>
+    </div>
+</body>
+</html>
